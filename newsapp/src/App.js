2bd9ad12891c37e38814c2bfb395b58f546e44f2
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-02 12:05:49 +0530

    initial commit of newsapp

---
+++ newsapp/src/App.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
+    )
+  }
+}
+
+// 9856e4b407c245f294e49ed3ace91c5b API KEY
+
+
+
