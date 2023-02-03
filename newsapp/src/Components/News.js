4e3fac19eb5c9596fc10186054a3a039ff8d5411
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-03 18:09:42 +0530

    In the process of making Previous and Next Buttons for page change

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -3,17 +3,26 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
 
-   
+    previousPageHandler=()=>{
+        console.log("next")
+    }
+
+    nextPageHandler=()=>{
+        console.log("next")
+    }
+
     constructor() {
         super()
         this.state = {
-            articles:[],
-            loading: false
+            articles: [],
+            loading: false,
+            page: 1
         }
     }
-    async componentDidMount(){
+    async componentDidMount() {
         console.log("cdm");
-        let url = "https://newsapi.org/v2/top-headlines?q=india&apiKey=9856e4b407c245f294e49ed3ace91c5b";
+        let url = "https://newsapi.org/v2/top-headlines?q=us&apiKey=9856e4b407c245f294e49ed3ace91c5b";
+        // &page=${this.state.page + 1}&pagesize=20`
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
@@ -21,17 +30,23 @@ export class News extends Component {
     }
     render() {
         return (
-            <div className="container my-3">
-                <h1>News Component</h1>
-                <div className="row">
-                    {this.state.articles.map((element) => {
-                        return (<div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title? element.title.slice(0, 45):""} description={element.description? element.description.slice(0, 88):""} imageUrl={element.urlToImage }newsUrl={element.url} />
-                        </div>)
-                    })}
-                </div>
+            <>
+                <div className="container my-3">
+                    <h1 className="my-4">Top Headlines</h1>
+                    <div className="row">
+                        {this.state.articles.map((element) => {
+                            return (<div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            </div>)
+                        })}
+                    </div>
 
-            </div>
+                </div>
+                <div className="container d-flex justify-content-between my-5">
+                    <button type="button" disabled={this.state.page<=1} className="btn btn-primary" onClick={this.previousPageHandler}>&larr;Previous</button>
+                    <button type="button" className="btn btn-primary" onClick={this.nextPageHandler}>Next&rarr;</button>
+                </div>
+            </>
         )
     }
 }
