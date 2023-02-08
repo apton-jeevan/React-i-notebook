bc74fe30b6354837d3981bd19ca98d0096d0bf61
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-08 12:15:46 +0530

    different routes set for different categories

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,18 +3,36 @@ import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
 
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
   render() {
     return (
       <div>
+        <BrowserRouter>
+
         <Navbar/>
-        <News pageSize={20} country={'in'} category={"sports"}/>
+        <Routes>
+        <Route exact path="/" element={ <News key="general" pageSize={20} country={'in'} category={"general"}/>}/>
+        <Route exact path="/business" element={ <News key="business" pageSize={20} country={'in'} category={"business"}/>}/>
+        <Route exact path="/entertainment" element={ <News key="entertainment" pageSize={20} country={'in'} category={"entertainment"}/>}/>
+        <Route exact path="/health" element={ <News key="health" pageSize={20} country={'in'} category={"health"}/>}/>
+        <Route exact path="/science" element={ <News key="science" pageSize={20} country={'in'} category={"science"}/>}/>
+        <Route exact path="/sports" element={ <News key="sports" pageSize={20} country={'in'} category={"sports"}/>}/>
+        <Route exact path="/technology" element={ <News key="technology" pageSize={20} country={'in'} category={"technology"}/>}/>
+        </Routes>
+        </BrowserRouter>
       </div>
     )
   }
 }
 
-// 9856e4b407c245f294e49ed3ace91c5b API KEY
+
 
 
 
--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -3,18 +3,19 @@ import Loading from './Loading';
 import NewsItem from './NewsItem';
 import PropTypes from 'prop-types';
 
+
 export class News extends Component {
     static defaultProps = {
         country: 'in',
-        pageSize: 8, 
+        pageSize: 8,
         category: 'general',
-      }
+    }
 
-      static propTypes = {
+    static propTypes = {
         country: PropTypes.string,
-        pageSize: PropTypes.number, 
+        pageSize: PropTypes.number,
         category: PropTypes.string,
-      }
+    }
 
     constructor() {
         super()
@@ -28,15 +29,15 @@ export class News extends Component {
 
     async componentDidMount() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=12e1c1efcb774d2995e47f59229b3950&page=1&pagesize=20`;
-    
-        this.setState({loading:true})//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
+
+        this.setState({ loading: true })//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
 
         let data = await fetch(url);
         let parsedData = await data.json()
-        this.setState({ 
-            articles: parsedData.articles ,
-            totalResults:parsedData.totalResults,
-            loading:false // as data has been fetched so we now make loading:false
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false // as data has been fetched so we now make loading:false
         })
 
     }
@@ -44,31 +45,31 @@ export class News extends Component {
 
     previousPageHandler = async () => {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=12e1c1efcb774d2995e47f59229b3950&page=${this.state.page - 1}&pagesize=20`;
-        this.setState({loading:true})//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
+        this.setState({ loading: true })//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles,  
-            loading:false// as data has been fetched so we now make loading:false
+            articles: parsedData.articles,
+            loading: false// as data has been fetched so we now make loading:false
         })
 
     }
 
     nextPageHandler = async () => {
-      
 
-      
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=12e1c1efcb774d2995e47f59229b3950&page=${this.state.page + 1}&pagesize=20`;
-            this.setState({loading:true})//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
-            let data = await fetch(url);
-            let parsedData = await data.json()
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,  
-                loading:false   // as data has been fetched so we now make loading:false
-            })  
-       
+
+
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=12e1c1efcb774d2995e47f59229b3950&page=${this.state.page + 1}&pagesize=20`;
+        this.setState({ loading: true })//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles,
+            loading: false   // as data has been fetched so we now make loading:false
+        })
+
 
     }
 
@@ -76,13 +77,13 @@ export class News extends Component {
     render() {
         return (
             <>
-            {/* Loading component should only be displayed when state variable 'loading is true' */}
-            {this.state.loading&&<Loading/>}
+                {/* Loading component should only be displayed when state variable 'loading is true' */}
+                {this.state.loading && <Loading />}
                 <div className="container my-3">
-                    <h1 className=" text-center" style={{margin:"50px"}}>Top Headlines</h1>
+                    <h1 className=" text-center" style={{ margin: "50px" }}>JK NewsFlash - Top Headlines</h1>
                     <div className="row">
                         {/* When loading is true  and Loading Component is displayed we do not want the data of earlier page to be shown */}
-                        {!this.state.loading&&this.state.articles.map((element) => {  
+                        {!this.state.loading && this.state.articles.map((element) => {
                             return (<div className="col-md-4" key={element.url}>
                                 <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                             </div>)
@@ -92,7 +93,7 @@ export class News extends Component {
                 </div>
                 <div className="container d-flex justify-content-between my-5">
                     <button type="button" disabled={this.state.page <= 1} className="btn btn-primary" onClick={this.previousPageHandler}>&larr; Previous</button>
-                    <button type="button"disabled={this.state.page + 1> Math.ceil(this.state.totalResults/20)} className="btn btn-primary" onClick={this.nextPageHandler}>Next &rarr;</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} className="btn btn-primary" onClick={this.nextPageHandler}>Next &rarr;</button>
                 </div>
             </>
         )
