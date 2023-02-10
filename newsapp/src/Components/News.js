c47aa090fec94536a95a71bac27869bc5a3158b3
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-10 10:52:08 +0530

    top-loading bar added

--- newsapp/src/Components/News.js
+++ newsapp/src/Components/News.js
@@ -1,13 +1,117 @@
+
+// PREVIOUS AND NEXT PAGE BUTTON
+
+// import React, { Component } from 'react';
+// import Loading from './Loading';
+// import NewsItem from './NewsItem';
+// import PropTypes from 'prop-types';
+
+
+// export class News extends Component {
+//     static defaultProps = {
+//         country: 'in',
+//         pageSize: 8,
+//         category: 'general',
+//     }
+
+//     static propTypes = {
+//         country: PropTypes.string,
+//         pageSize: PropTypes.number,
+//         category: PropTypes.string,
+//     }
+//     capitalizeFirstLetter= (string)=> {
+//         return string.charAt(0).toUpperCase() + string.slice(1);
+//     }
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             articles: [],
+//             loading: false,
+//             page: 1,
+//             totalResults:0
+//         }
+//         document.title=`${this. capitalizeFirstLetter(this.props.category)} - JK NewsApp`
+//     }
+
+
+//     async newsPageUpdate() {
+//         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9856e4b407c245f294e49ed3ace91c5b&page=${this.state.page}&pagesize={this.props.pageSize}`;
+
+//         this.setState({ loading: true })//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
+
+//         let data = await fetch(url);
+//         let parsedData = await data.json()
+//         this.setState({
+//             articles: parsedData.articles,
+//             totalResults: parsedData.totalResults,
+//             loading: false // as data has been fetched so we now make loading:false
+//         })
+//     }
+
+//     async componentDidMount() {
+//         this.newsPageUpdate();
+
+//     }
+
+
+//     previousPageHandler = async () => {
+//         await this.setState({ page: this.state.page - 1 });
+//         this.newsPageUpdate();
+//     }
+
+//     nextPageHandler = async () => {
+//         await this.setState({ page: this.state.page + 1 });
+//         this.newsPageUpdate();
+
+//     }
+
+
+//     render() {
+//         return (
+//             <>
+//                 {/* Loading component should only be displayed when state variable 'loading is true' */}
+//                 {this.state.loading && <Loading />}
+//                 <div className="container my-3">
+//                     <h1 className=" text-center" style={{ margin: "50px" }}>JK NewsFlash - Top Headlines on {this. capitalizeFirstLetter(this.props.category)}</h1>
+//                     <div className="row">
+//                         {/* When loading is true  and Loading Component is displayed we do not want the data of earlier page to be shown */}
+//                         {!this.state.loading && this.state.articles.map((element) => {
+//                             return (<div className="col-md-4" key={element.url}>
+//                                 <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+//                             </div>)
+//                         })}
+//                     </div>
+
+//                 </div>
+//                 <div className="container d-flex justify-content-between my-5">
+//                     <button type="button" disabled={this.state.page <= 1} className="btn btn-primary" onClick={this.previousPageHandler}>&larr; Previous</button>
+//                     <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-primary" onClick={this.nextPageHandler}>Next &rarr;</button>
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+
+// export default News
+
+
+
+
+
+
+// INFINITY SCROLL DOWN
 import React, { Component } from 'react';
 import Loading from './Loading';
 import NewsItem from './NewsItem';
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 export class News extends Component {
     static defaultProps = {
         country: 'in',
-        pageSize: 8,
+        pageSize:20,
         category: 'general',
     }
 
@@ -16,7 +120,7 @@ export class News extends Component {
         pageSize: PropTypes.number,
         category: PropTypes.string,
     }
-    capitalizeFirstLetter= (string)=> {
+    capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
     constructor(props) {
@@ -24,65 +128,80 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page: 1
+            page: 1,
+            totalResults: 0
         }
-        document.title=`${this. capitalizeFirstLetter(this.props.category)} - JK NewsApp`
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - JK NewsApp`
     }
-     
 
-    async newsPageUpdate() {
+
+     newsPageUpdate=async ()=> {
+        this.props.setProgress(10)
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9856e4b407c245f294e49ed3ace91c5b&page=${this.state.page}&pagesize={this.props.pageSize}`;
 
-        this.setState({ loading: true })//loading is made true when we are going to hit the apit thata is when we are going to fetch the url
+        this.setState({ loading: true })
+        //loading is made true when we are going to hit the apit thata is when we are going to fetch the url
 
         let data = await fetch(url);
+
+        this.props.setProgress(30)
+
         let parsedData = await data.json()
+
+        this.props.setProgress(70)
+
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false // as data has been fetched so we now make loading:false
         })
+        this.props.setProgress(100)
     }
 
     async componentDidMount() {
         this.newsPageUpdate();
-
     }
 
 
-    previousPageHandler = async () => {
-        await this.setState({ page: this.state.page - 1 });
-        this.newsPageUpdate();
+    fetchMoreData= async () => {
+
+        this.setState({page:this.state.page+1 })    
+
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9856e4b407c245f294e49ed3ace91c5b&page=${this.state.page +1}&pagesize={this.props.pageSize}`;
+
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults   
+        })
     }
-
-    nextPageHandler = async () => {
-        await this.setState({ page: this.state.page + 1 });
-        this.newsPageUpdate();
-
-    }
-
 
     render() {
         return (
             <>
                 {/* Loading component should only be displayed when state variable 'loading is true' */}
                 {this.state.loading && <Loading />}
+                    <h1 className=" text-center" style={{ margin: "50px" }}>JK NewsFlash - Top Headlines on {this.capitalizeFirstLetter(this.props.category)}</h1>
+                        <InfiniteScroll
+                            dataLength={this.state.articles.length}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.articles.length !== this.state.totalResults}
+                            loader={<Loading />}
+                        >
                 <div className="container my-3">
-                    <h1 className=" text-center" style={{ margin: "50px" }}>JK NewsFlash - Top Headlines on {this. capitalizeFirstLetter(this.props.category)}</h1>
                     <div className="row">
-                        {/* When loading is true  and Loading Component is displayed we do not want the data of earlier page to be shown */}
-                        {!this.state.loading && this.state.articles.map((element) => {
-                            return (<div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                            </div>)
-                        })}
+                            {/* When loading is true  and Loading Component is displayed we do not want the data of earlier page to be shown */}
+                            {this.state.articles.map((element) => {
+                                return (<div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>)
+                            })}
                     </div>
+                </div>
+                        </InfiniteScroll>
 
-                </div>
-                <div className="container d-flex justify-content-between my-5">
-                    <button type="button" disabled={this.state.page <= 1} className="btn btn-primary" onClick={this.previousPageHandler}>&larr; Previous</button>
-                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-primary" onClick={this.nextPageHandler}>Next &rarr;</button>
-                </div>
+
             </>
         )
     }
@@ -90,4 +209,5 @@ export class News extends Component {
 
 
 export default News
+
 
--- newsapp/src/Components/NewsItem.js
+++ newsapp/src/Components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
                     <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger"style={{zIndex:"1", left:"80%"}}>
                        {source}
                     </span>
-                    <img src={imageUrl ? imageUrl : "https://gumlet.assettype.com/barandbench%2F2021-01%2F47f6c197-325e-4776-86e5-354fbb11fee1%2FElection__1_.jpg?w=1200&auto=format%2Ccompress&ogImage=true&enlarge=true"} style={{ height: "200px" }} className="card-img-top" />
+                    <img src={imageUrl?imageUrl:"https://gumlet.assettype.com/barandbench%2F2021-01%2F47f6c197-325e-4776-86e5-354fbb11fee1%2FElection__1_.jpg?w=1200&auto=format%2Ccompress&ogImage=true&enlarge=true"} style={{ height: "200px" }} className="card-img-top" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
