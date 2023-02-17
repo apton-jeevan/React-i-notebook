b04276485c54c0507bafcd596587bdb208db4c75
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-17 17:22:26 +0530

    Password Hashing using bcrypt npm and Json Web Token using jwt npm

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,7 +2,8 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 //Express Validator     
 
 //creating route api/auth/createUser so that user created
@@ -10,22 +11,35 @@ router.post('/createUser',
 [body('email',"Enter a valid email").isEmail(),body('password',"password should be atleast 5 characters").isLength({ min: 5 }),body('name',"name should be atleast 3 characters").isLength({ min: 3 })]
 , async (req, res)=>{
   // if there are errors in validation then return bad response with the error
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    //Most driver methods that communicate with your MongoDB cluster, such as findOneAndUpdate() and countDocuments() SEE MONGODB DOCUMENTATION
-   
-    let user= await User.findOne({email:req.body.email})
-if (user){
-  return res.status(400).json({error:"sorry user with this email id already exists"})
-}
-   user= await User.create({
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  //Most driver methods that communicate with your MongoDB cluster, such as findOneAndUpdate() and countDocuments() SEE MONGODB DOCUMENTATION
+  
+  let user= await User.findOne({email:req.body.email})
+  if (user){
+    return res.status(400).json({error:"sorry user with this email id already exists"})
+  }
+  
+  const salt = await bcrypt.genSalt(10);
+  const secPass = await bcrypt.hash(req.body.password, salt);
+
+
+  //storing user's data in monogo db
+  user= await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password:secPass ,
         email:req.body.email
       })
-      res.json(user)    // sending response to thunder client that user has been created 
+
+      const data={
+        id:user.id
+      }
+
+  const JWT_SECRET = 'Harryisagoodboy';
+  const authtoken= jwt.sign(data,JWT_SECRET);
+      res.json({authtoken:authtoken})    // sending response to thunder client that user has been created 
    
 } )
 
