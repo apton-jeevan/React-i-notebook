865dbee784a3a91001ce145aba3192e3f3e8f811
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-20 11:49:56 +0530

    use location

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -8,20 +8,20 @@ import './App.css';
 import About from "./Components/About";
 import Home from "./Components/Home";
 import Navbar from './Components/Navbar';
-import NoteState from "./Context/NoteState";
+// import NoteState from "./Context/NoteState";
 
 function App() {
   return (
     <>
-      <NoteState>
         <Router>
           <Navbar />
+          <div className="container my-4">
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
           </Routes>
+          </div>
         </Router>
-      </NoteState>
     </>
   );
 }
--- inotebook/src/Components/About.js
+++ inotebook/src/Components/About.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import NoteContext from '../Context/notes/NoteContext'
-import { useContext } from 'react'
-import { useEffect } from 'react'
+// import NoteContext from '../Context/notes/NoteContext'
+// import { useContext } from 'react'
+// import { useEffect } from 'react'
 export default function About() {
-
-  const a = useContext(NoteContext)
-
-  useEffect(() => {
-      a.update()
-  })
-
   return (
     <div>
       <h1>This is About Page</h1>
-      This is About {a.state.name} and he is in class {a.state.class}
-
     </div>
   )
 }
