e0b220abd812964d9a881191a5124c849d8f142c
apton-jeevan <jeevan.kurian@aptonworks.com>
2023-02-26 14:59:12 +0530

    Successfully implemented add,delete,fetch

--- inotebook/src/Components/NoteItem.js
+++ inotebook/src/Components/NoteItem.js
@@ -3,7 +3,7 @@ import NoteContext from '../Context/notes/NoteContext'
 
 export default function NoteItem(props) {
     const context = useContext(NoteContext)
-    const { DeleteNote }=context
+    const { DeleteNote,EditNote }=context
     const { note } = props
 
     return (
@@ -12,8 +12,9 @@ export default function NoteItem(props) {
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
+                    <p className="card-text">{note._id}</p>
                     <i className="fa-solid fa-trash me-3" onClick={()=>{DeleteNote(note._id)}}></i>
-                    <i className="fa-sharp fa-solid fa-file-pen"></i>
+                    <i className="fa-sharp fa-solid fa-file-pen" onClick={()=>{EditNote(note._id)}}></i>
                 </div>
             </div>
         </div>
--- inotebook/src/Context/NoteState.js
+++ inotebook/src/Context/NoteState.js
@@ -19,7 +19,7 @@ export default function NoteState(props) {
                 "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNmMDZhMmZmOGE1ZTlkYmM2ZDI2MjA5In0sImlhdCI6MTY3Njc4NDg1M30.6mz3VoiyAdw91NNiXlp4mhVL0UX3trD7Ptpvxej4bxo"
             }
         });
-        const json = await response.json()
+        const json = await response.json() // await used as response.json is a promise.
         setNotes(json) // with this all notes of a particular user will be fetched
     }
 
@@ -36,21 +36,29 @@ export default function NoteState(props) {
             body: JSON.stringify({ title, description, tag })
 
         });
+           getNotes(); // to fetch the newly added note 
+    }
+
+        
+  // DO NOT USE THE BELOW METHOD FOR ADDING NOTES IN CLIENT AS THE NEWLY ADDED NOTE HERE IS NOT FETCHED FROM DATABASE. 
+  //SO THE ADDED NOTE WHICH WE ARE SEEING HAS ONLY TITLE ,DESCRIPTION AND TAG WHICH WE HAVE GIVEN AND THE REMAINGING DATE,ID ETC. ARE NOT SET IN CLIENT SIDE. 
+  //THAT IS WHY WHEN WE TRY TO DELETE THE NOTE AS SOON AS WE ADD IT WE GET THE ERROR 'Cast to ObjectId failed for value "undefined" (type string) at path "_id" for model "Note" . IT IS ONLY AFTER REFRESHING WE CAN DELETE THE NEWLY ADDED NOTE AS NOW THE NEWLY ADDED NOTE IS FETECHED FROM DATABASE DUE TO REFRESHING BCZ WITH REFRESH AGAIN UseEffect() is called WHERE GETNOTES IS DEFINED
 
         // to add notes in the client side
-        const note = {
-            "title": title,
-            "description": description,
-            "tag": tag,
-        };
-        setNotes(notes.concat(note))
-    }
+
+        // const note = {
+        //     "title": title,
+        //     "description": description,
+        //     "tag": tag,                    
+        // };
+        // setNotes(notes.concat(note))
 
 
     //Edit Note
+
     const EditNote = async (id, title, description, tag) => {
         // API Call 
-        const response = await fetch(`${host}/api/note/updatenote/${id}`, {
+    await fetch(`${host}/api/note/updatenote/${id}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -58,27 +66,37 @@ export default function NoteState(props) {
             },
             body: JSON.stringify({ title, description, tag })
         });
-        const json = response.json();
+        getNotes()
+
 
         // To edit in client side
-        for (let i = 0; i < notes.length; i++) {
-            const element = notes[i];
-            if (element._id === id) {
-                element.title = title;
-                element.description = description;
-                element.tag = tag;
-            }
-        }
+        // for (let i = 0; i < notes.length; i++) {
+        //     const element = notes[i];
+        //     if (element._id === id) {
+        //         element.title = title;
+        //         element.description = description;
+        //         element.tag = tag;
+        //     }
+        // }
 
     }
-    const DeleteNote = (id) => {
-        console.log("delete")
-        const NewNotes = notes.filter((note) => { return note._id !== id })
-        setNotes(NewNotes)
+    const DeleteNote = async (id) => {
+        await fetch(`${host}/api/note/deletenote/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjNmMDZhMmZmOGE1ZTlkYmM2ZDI2MjA5In0sImlhdCI6MTY3Njc4NDg1M30.6mz3VoiyAdw91NNiXlp4mhVL0UX3trD7Ptpvxej4bxo"
+            }
+
+        })  
+
+        getNotes();   // to fetch the notes after we have deleted the note from database
+       
     }
+
     return (
         <div>
-            <NoteContext.Provider value={{ notes, AddNoteApi, DeleteNote, getNotes }}>
+            <NoteContext.Provider value={{ notes, AddNoteApi, DeleteNote, getNotes,EditNote }}>
                 {props.children}
             </NoteContext.Provider>
         </div>
